4f92077b1104521f80dc1f900b07a9aed3f4d6ad
GOPAL THAKUR <gopalk557@gmail.com>
2022-02-12 00:46:49 +0530

    [Gopal]Add:Compare Line

--- Program.cs
+++ Program.cs
@@ -15,15 +15,24 @@ namespace LineCompareProblem
             line1.CalculateLength();
             LineCompare line2 = new LineCompare(1, 2, 4, 5);
             line2.CalculateLength();
-           
+
             if (line1.Equals(line2))
+                if (line1.CalculateLength() > line2.CalculateLength())
+                {
+                    Console.WriteLine("Lines are  equal \n");
+                    Console.WriteLine("Line1 is greater than line2 \n");
+                }
+                else
+                if (line1.CalculateLength() < line2.CalculateLength())
+                {
+                    Console.WriteLine("Lines are not equal \n");
+                    Console.WriteLine("Lines1 is smaller than Line2 \n");
+                }
+            if (line1.CalculateLength() == line2.CalculateLength())
             {
-                Console.WriteLine("Lines are  equal \n");
+                Console.WriteLine("Line1 are line2 are equal \n");
             }
-            else
-            {
-                Console.WriteLine("Lines are not equal \n");
-            }
+            Console.ReadLine();
         }
     }
 }
